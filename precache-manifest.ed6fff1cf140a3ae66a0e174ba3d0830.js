e0ac9df1536cbbd4d26e2bc002c727bc1103dd61
EgorChaban <Egorkachibo@mail.ru>
2021-03-15 14:03:56 +0300

    Updates

---
+++ precache-manifest.ed6fff1cf140a3ae66a0e174ba3d0830.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bc1eecd6c8a6d38e88898fae6bbc017",
+    "url": "/TodoList/index.html"
+  },
+  {
+    "revision": "f90c1dd519833a7ead5b",
+    "url": "/TodoList/static/css/main.aa207b39.chunk.css"
+  },
+  {
+    "revision": "c49fb207e77e7eda00ea",
+    "url": "/TodoList/static/js/2.9df2edcc.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/TodoList/static/js/2.9df2edcc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f90c1dd519833a7ead5b",
+    "url": "/TodoList/static/js/main.4440cd42.chunk.js"
+  },
+  {
+    "revision": "cec92663b5aa42fa8dea",
+    "url": "/TodoList/static/js/runtime-main.46cf6363.js"
+  }
+]);
